22e6ecd87b65aceac4158ea66750f0149b2a52f2
marcusworld82 <had2work925@GMAIL.COM>
2025-08-16 12:01:17 -0700

    Fix Client Component Error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f3363b77999da51"; }
+/******/ 		__webpack_require__.h = function() { return "c6bf1c9015511e74"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
