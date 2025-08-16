6fd6543b03c13633565630863753ae5ede3b9dfe
marcusworld82 <had2work925@GMAIL.COM>
2025-08-15 17:49:03 -0700

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fedca4ca8909344e"; }
+/******/ 		__webpack_require__.h = function() { return "4764d2c292c31191"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
